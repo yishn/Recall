7d69e09e04fd7c5472e52bcd4771b02a4a807f7c
Yichuan Shen <shen.yichuan@gmail.com>
2016-03-07 23:55:34 +0100

    Add some Set methods

--- classes/Set.php
+++ classes/Set.php
@@ -4,4 +4,19 @@ class Set extends Model {
     public function get_vocabularies() {
         return $this->has_many('Vocabulary');
     }
+
+    public function get_new_vocabularies() {
+        $max = Setting::get('new_cards_per_day');
+        $learned = Vocabulary::filter('learned_today')->count();
+
+        return $this->get_vocabularies()->filter('inactive')->limit($max - $learned)->find_many();
+    }
+
+    public function get_due_count() {
+        return $this->get_vocabularies()->filter('due')->count();
+    }
+
+    public function get_permalink() {
+        return BASE_PATH . 'set/' . $this->id;
+    }
 }
