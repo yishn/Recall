70335f3bf5e0cbbb7d98163360ab91d2c3f9f113
Yichuan Shen <shen.yichuan@gmail.com>
2016-03-07 22:00:57 +0100

    Add libs and basic structure

---
+++ config-demo.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Edit this file and save it under `config.php`.
+ */
+
+// MySQL settings
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+define('DB_PREFIX', '');
+
+/**
+ * Ok, stop editing now!
+ */
+
+// Configure Idiorm
+ORM::configure([
+    'connection_string' => 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
+    'username' => DB_USER,
+    'password' => DB_PASS,
+    'driver_options' => array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'),
+    'caching' => true,
+    'caching_auto_clear' => true
+]);
+
+// Configure Paris
+Model::$auto_prefix_tables = DB_PREFIX;
