f41800fbaadaa0d9a0b4ecf3c7e9f78774ca63bc
Yichuan Shen <shen.yichuan@gmail.com>
2016-03-07 23:43:34 +0100

    Add Setting class

---
+++ classes/Setting.php
@@ -0,0 +1,12 @@
+<?php
+
+class Setting {
+    private static $data = [
+        'new_cards_per_day': 20,
+        'intervals': [4, 8, 24, 3*24, 7*24, 14*24, 30*24, 120*24]
+    ];
+
+    public get($key) {
+        return self::$data[$key];
+    }
+}
