36e69b68dfe4fe0b2d976ad5df19cabc84ea5f3d
Yichuan Shen <shen.yichuan@gmail.com>
2016-03-08 00:20:08 +0100

    Silly dictionary syntax

--- classes/Setting.php
+++ classes/Setting.php
@@ -2,8 +2,8 @@
 
 class Setting {
     private static $data = [
-        'new_vocabs_per_day': 20,
-        'intervals': [4, 8, 24, 3*24, 7*24, 14*24, 30*24, 120*24]
+        'new_vocabs_per_day' => 20,
+        'intervals' => [4, 8, 24, 3*24, 7*24, 14*24, 30*24, 120*24]
     ];
 
     public static function get($key) {
