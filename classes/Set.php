2d64960032db47123af2bc75298563e71cfd7a1b
Yichuan Shen <shen.yichuan@gmail.com>
2016-03-08 13:06:01 +0100

    Update get_new_vocabularies()

--- classes/Set.php
+++ classes/Set.php
@@ -6,10 +6,13 @@ class Set extends Model {
     }
 
     public function get_new_vocabularies() {
-        $max = Setting::get('new_vocabs_per_day');
-        $learned = Vocabulary::filter('learned_today')->count();
+        $max = Setting::get('new_vocabs_per_day') - Vocabulary::filter('learned_today')->count();
+        $sets = Vocabulary::filter('inactive')->select('set_id')->limit($max)->distinct()->find_many();
 
-        return $this->get_vocabularies()->filter('inactive')->limit($max - $learned)->find_many();
+        if (count($sets) == 0 || !in_array($this->id, array_map(function($x) { return $x->set_id; }, $sets))) return [];
+
+        $limit = round($max / count($sets));
+        return Set::get_vocabularies()->filter('inactive')->limit($limit)->find_many();
     }
 
     public function get_due_count() {
