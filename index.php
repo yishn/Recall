b86c4a20b450da7257e554475a9a10895b36d2bb
Yichuan Shen <shen.yichuan@gmail.com>
2016-03-08 12:21:40 +0100

    Update templates

--- index.php
+++ index.php
@@ -4,18 +4,20 @@ require_once('includes.php');
 
 route('GET', '/', function() {
     return response(phtml('view/dashboard', [
+        'title' => 'Dashboard',
         'sets' => Set::order_by_asc('name')->find_many(),
         'set' => null
     ]));
 });
 
+route('GET', '/error', page('view/error', ['title' => 'Error']));
+
 route('GET', '/set/:id', function($args) {
     $set = Set::find_one($args['id']);
 
-    if (!$set) return response(phtml('view/error'));
+    if (!$set) return redirect(BASE_PATH . 'error');
 
-    return response(phtml('view/dashboard', [
-        'sets' => Set::order_by_asc('name')->find_many(),
+    return response(phtml('view/set', [
         'set' => $set
     ]));
 });
